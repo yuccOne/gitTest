cee861b2ab594b737ef63c635cd2a9f77c015b9a
sullimygirl <sulli@onlyone.com>
2021-08-20 20:31:25 +0800

    master commit2

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git4!");
+        System.out.println("master test");
     }
 }
